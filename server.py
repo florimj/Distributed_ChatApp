7f75a758f22f9262890c3a4d270f8ee12b698385
LouiseAnumu <Louise.Anumu@Student.Reutlingen-University.DE>
2025-01-27 12:31:36 +0100

    Update server.py
    finale Anpassungen

--- server.py
+++ server.py
@@ -43,12 +43,18 @@ class ChatServer:
         threading.Thread(target=self.monitor_heartbeat).start()
         threading.Thread(target=self.broadcast_discovery).start()
 
+        # Delay election to allow discovery
+        time.sleep(10)
+        print("Initiating leader election at startup...")
+        self.initiate_leader_election()
+          
       def broadcast_discovery(self):
         while True:
             discover_message = {
                 "type": "discover",
                 "id": self.id,
-                "port": self.port
+                "port": self.port,
+                "isLeader": self.is_leader
             }
             self.discovery_socket.sendto(json.dumps(discover_message).encode(),
                                 ('<broadcast>', self.discovery_port))
@@ -69,7 +75,7 @@ class ChatServer:
 
       def monitor_heartbeat(self):
         """Monitor leader's heartbeat and trigger election on failure."""
-        while True:
+          while True:
             time.sleep(10)  # Check every 10 seconds
             if not self.is_leader and (time.time() - self.last_heartbeat > 20):  # 20-second timeout
                 print("Leader unresponsive. Initiating leader election.")
@@ -94,6 +100,7 @@ class ChatServer:
                         "isLeader": data['isLeader']
                     }
                     print(f"Discovered new server: {server_ip}:{server_port}")
+                    
               elif data["type"] == "leader":
                 leader_id = server_id
                 server_port = data ['port']
@@ -102,6 +109,64 @@ class ChatServer:
                 #update status
                 self.is_leader = (leader_id == self.id)
                 self.voted = False
+
+                
+                # Update the respective server entry with `isLeader=True`
+                if leader_id in self.known_servers:
+                    self.known_servers[leader_id]["isLeader"] = True
+
+                else:
+                    print(f"Leader {leader_id} not found in known servers. Adding it.")
+                    # Add leader to the known_servers if not already present
+                    self.known_servers[leader_id] = {
+                        "id": leader_id,
+                        "ip": address[0],
+                        "port": server_port,
+                        "isLeader": True
+                    }
+
+            elif data["type"] == "heartbeat":
+                    leader_id = server_id
+                    if leader_id == self.id: # Ignore heartbeats from self
+                        return  
+                    print(f"Heartbeat received from leader {server_ip}:{server_port}.")
+                    self.last_heartbeat = time.time()  # Update the last heartbeat timestamp
+
+
+    def forward_token(self, token_id):
+        """Send the election token to the next server in the logical ring."""
+        sorted_servers = sorted(self.known_servers.values(), key=lambda x: x["id"])
+        my_index = next((i for i, server in enumerate(sorted_servers) if server["id"] == self.id), None)
+
+        if my_index is None:
+            print("Error: This server is not in the known_servers list.")
+            return
+
+        # Find the next server in the ring
+        next_server = sorted_servers[(my_index + 1) % len(sorted_servers)]
+        next_address = (next_server["ip"], next_server["port"])
+
+        try:
+            print(f"Forwarding token {token_id} to {next_server['id']} at {next_address}.")
+            self.server_socket.sendto(json.dumps({"type": "election", "token": token_id}).encode(), next_address)
+        
+        except:
+            print(f"Failed to send token {token_id} to {next_server['id']} at {next_address}")
+            print(f"Removing server {next_server['id']} from known servers due to failure.")
+            self.known_servers.pop(next_server["id"], None)  # Remove the unreachable server
+
+
+    def broadcast_leader(self):
+        """Broadcast leader information to all known servers."""
+        for server in self.known_servers.values():
+            leader_message = {
+                "type": "leader",
+                "id": self.id,
+                "port":self.port
+            }
+            print(f"Leader {self.id} will be broadcasted to all known servers.")
+            self.discovery_socket.sendto(json.dumps(leader_message).encode(), ('<broadcast>', self.discovery_port))
+        
 
       def listen_on_server_port(self):
           while True:
@@ -128,42 +193,57 @@ class ChatServer:
 
                     print(f"Client {data['id']} connected from {address}")
 
-                    elif data["type"] == "message":
+                elif data["type"] == "message":
                     # Forward message
                     print(f"Received message from client {data['id']}: {data['text']}")
                     self.broadcast_message(data, data['id'])
+                  
+                elif data["type"] == "election":
+                    token_id = data["token"]
+                    print(f"Received election token {token_id} from {address}.")
+                    if (self.voted==False):
+                        if token_id > self.id:
+                            # Forward the token unchanged
+                                self.forward_token(token_id)
+                        elif token_id < self.id:
+                            # Replace with this server's ID and forward
+                            self.forward_token(self.id)
+                        elif token_id == self.id:
+                            # This server is the leader
+                            print(f"I have won the election!")
+                            self.is_leader = True
+                            self.broadcast_leader()
+                            threading.Thread(target=self.broadcast_heartbeat).start()
+                    else:
+                        print("I already voted and my own token is the highest. I am the leader now!")
+                        self.broadcast_leader()   
+                        threading.Thread(target=self.broadcast_heartbeat).start()
+                self.voted = True
+                
+            except Exception as e:
+                print("Someone disconnected!")
+                print(e)
+                
+                # Find the next server in the ring
+                sorted_servers = sorted(self.known_servers.values(), key=lambda x: x["id"])
+                my_index = next((i for i, server in enumerate(sorted_servers) if server["id"] == self.id), None)
+                next_server = sorted_servers[(my_index + 1) % len(sorted_servers)]
+                next_address = (next_server["ip"], next_server["port"])
+                print(f"Remove {next_address} from known servers list")
+                self.known_servers.pop(next_server["id"], None)  # Remove the unreachable server
 
-      def listen_on_discovery_port(self):
-        """Listen for discovery messages on the discovery port."""
-        while True:
-            message, address = self.discovery_socket.recvfrom(1024)
-            data = json.loads(message.decode())
-            server_id = data['id']
-            server_ip = address[0]
-            
-            if data["type"] == "discover":
-                print(f"Discovered new server: {server_ip}:{server_port}")
-                    self.known_servers[server_id] = {
-                        "id": server_id,
-                        "ip": server_ip,
-                        "port": server_port,
-                        "isLeader": data['isLeader']
-                        
-            if data["type"] == "election":
-                        print(f"Received election token {data['token']} from {address}")
-
-
-      def forward_token(self, token_id):
-          next_server = self.get_next_server()
-          self.server_socket.sendto(json.dumps({"type": "election", "token": token_id}).encode(), next_server)
-
+    
     def broadcast_message(self, message, sender):
         # Send message to all other clients
         for client in self.known_clients:
             if client != sender:  # Do not send the message back to the sender
                 adress = (self.known_clients[client]["ip"],
                           self.known_clients[client]["port"])
-                
+                try:
+                    self.server_socket.sendto(json.dumps(message).encode(), adress)
+                except Exception as e:
+                    print(e)
+                    
       def initiate_leader_election(self):
         print(f"Server {self.id} initiating leader election.")
         # Start the election with this server's ID
